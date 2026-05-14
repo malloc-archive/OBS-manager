fbfe68873827c50147e0e5f68af3a6086c4abca0
sfinaecpp <sfinaecpp@gmail.com>
2026-05-14 19:21:00 +0900

    new method used

--- plugin-main.c
+++ plugin-main.c
@@ -21,27 +21,81 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <util/dstr.h>
 #include <plugin-support.h>
 #include <obs-frontend-api.h>
+#include <util/config-file.h>
 #include <windows.h>
 #include <psapi.h>
+#include <string.h>
+#include <direct.h>
+#include <errno.h>
+#include <stdlib.h>
+
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
-char playing[256] = "No fullscreen app detected";
-char *game_list[] = {
-	"a",
-	"s"
-};
+wchar_t playing[520] = L"No fullscreen app detected";
+char *game_list[] = {"EscapeFromTarkov.exe", "s"};
 int size = 2;
-char *path = "";
+char real_name[256] = "";
 
-bool contains(char *current, char *list[], int size) {
+char *concat(const char *a, const char *b)
+{
+    size_t len_a = strlen(a);
+    size_t len_b = strlen(b);
+
+    char *result = (char *)malloc(len_a + len_b + 1);
+
+    if (!result)
+        return NULL;
+
+    strcpy(result, a);
+    strcat(result, b);
+
+    return result;
+}
+
+bool contains(char *current, char *list[], int size)
+{
 	for (int i = 0; i < size; i++) {
 		if (strcmp(current, list[i]) == 0) {
 			return true;
 		}
 	}
 	return false;
+}
+
+void get_real_name(LPWSTR path, LPWSTR name_buffer, DWORD buffer_size)
+{
+	LPWSTR last_backslash = wcsrchr(path, L'\\');
+	if (last_backslash) {
+		wcsncpy(name_buffer, last_backslash + 1, buffer_size - 1);
+		name_buffer[buffer_size - 1] = L'\0';
+	} else {
+		wcsncpy(name_buffer, path, buffer_size - 1);
+		name_buffer[buffer_size - 1] = L'\0';
+	}
+}
+
+char *normalize_path(const char *path)
+{
+	
+    if (!path || !*path)
+        return NULL;
+	char *temp = _strdup(path);
+    size_t len = strlen(temp);
+
+    size_t end = len;
+
+    if (end > 0 && (temp[end - 1] == '\\' || temp[end - 1] == '/'))
+        end--;
+
+    for (size_t i = end; i > 0; i--) {
+        if (temp[i - 1] == '\\' || temp[i - 1] == '/') {
+            return &temp[i];
+        }
+    }
+
+    return temp;
 }
 
 static void on_recording_stop(void *data, calldata_t *cd)
@@ -52,19 +106,15 @@ static void on_recording_stop(void *data, calldata_t *cd)
 	if (!path)
 		return;
 	obs_log(LOG_INFO, "recording saved to: %s", path);
-
-
-
 }
 
-void get_fullscreen_app_name(char *name_buffer, DWORD buffer_size)
+void get_fullscreen_app_name(LPWSTR name_buffer, DWORD buffer_size, char *path_buffer, DWORD path_buffer_size)
 {
 	HWND hwnd = GetForegroundWindow();
-    if (!hwnd) {
-    	strncpy(name_buffer, "No fullscreen app detected", buffer_size - 1);
-		name_buffer[buffer_size - 1] = '\0';
-    	return;
-	}	
+	if (!hwnd) {
+		wcscpy(name_buffer, L"No fullscreen app detected");
+		return;
+	}
 
 	RECT window_rect;
 	GetWindowRect(hwnd, &window_rect);
@@ -72,61 +122,104 @@ void get_fullscreen_app_name(char *name_buffer, DWORD buffer_size)
 	int screen_width = GetSystemMetrics(SM_CXSCREEN);
 	int screen_height = GetSystemMetrics(SM_CYSCREEN);
 
-	if((window_rect.right - window_rect.left) >= screen_width && (window_rect.bottom - window_rect.top) >= screen_height){
+	if ((window_rect.right - window_rect.left) >= screen_width &&
+	    (window_rect.bottom - window_rect.top) >= screen_height) {
 
 		DWORD process_id;
 		GetWindowThreadProcessId(hwnd, &process_id);
-		HANDLE process_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, process_id);
+		HANDLE process_handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, process_id);
 
 		if (process_handle) {
-    		if (GetModuleBaseNameA(process_handle, NULL, name_buffer, buffer_size) == 0) {
-				obs_log(LOG_ERROR, "Failed to get module base name: %lu", GetLastError());
-        		strncpy(name_buffer, "Unknown", buffer_size - 1);
-        		name_buffer[buffer_size - 1] = '\0';
+			DWORD size_needed = buffer_size;
+
+			if (!QueryFullProcessImageNameW(process_handle, 0, name_buffer, &size_needed)) {
+				obs_log(LOG_ERROR, "QueryFullProcessImageNameW failed: %lu", GetLastError());
+
+				wcscpy(name_buffer, L"Unknown");
+			} else {
+				get_real_name(name_buffer, name_buffer, size_needed);
+				char utf8[1024];
+				wcstombs(utf8, name_buffer, sizeof(utf8));
+				strncpy(path_buffer, utf8, path_buffer_size - 1);
+				path_buffer[path_buffer_size - 1] = '\0';
+				obs_log(LOG_INFO, "Fullscreen app detected: %s", utf8);
 			}
-    	CloseHandle(process_handle);
+			CloseHandle(process_handle);
 		} else {
-    		strncpy(name_buffer, "OpenProcess failed", buffer_size - 1);
-    		name_buffer[buffer_size - 1] = '\0';
+			//strncpy(name_buffer, "OpenProcess failed", buffer_size - 1);
+			//name_buffer[buffer_size - 1] = '\0';
+			obs_log(LOG_ERROR, "Failed to open process: %lu", GetLastError());
+			wcscpy(name_buffer, L"Unknown");
+			name_buffer[buffer_size - 1] = L'\0';
 		}
-	}
-	else {
-		strncpy(name_buffer, "No fullscreen app detected", buffer_size - 1);
-		name_buffer[buffer_size - 1] = '\0';
+	} else {
+		wcscpy(name_buffer, L"No fullscreen app detected");
+		name_buffer[buffer_size - 1] = L'\0';
 	}
 }
 static void on_recording_start(void *data, calldata_t *cd)
 {
+	config_t *config = obs_frontend_get_profile_config();
+	
+	const char *mode = config_get_string(config, "Output", "Mode"); //not sure with the section and name, need to check obs config file
+
+	const char *path = NULL;
+	
+
+	if (strcmp(mode, "Advanced") == 0) {
+    	path = config_get_string(config, "AdvOut", "RecFilePath");
+	} else {
+    	path = config_get_string(config, "SimpleOutput", "FilePath");
+	}
+	char *normalized_path = normalize_path(path);
+	obs_log(LOG_INFO, "Recording dir: %s", path);
+
+	obs_log(LOG_INFO, "Normalized recording dir: %s", normalized_path);
+	char *temp_path = "";
 
 	obs_log(LOG_INFO, "recording started");
-	get_fullscreen_app_name(playing, 256);
-	if (strcmp(playing, "No fullscreen app detected") == 0) {
-
-	} else if(contains(playing, game_list, size)) {
+	get_fullscreen_app_name(playing, 520, real_name, 256);
+	if (strcmp(real_name, "No fullscreen app detected") == 0 || strcmp(real_name, "Unknown") == 0) {
+		obs_log(LOG_INFO, "no supported fullscreen app detected");
+	} else if (contains(real_name, game_list, size)) {
+		obs_log(LOG_INFO, "detected supported fullscreen app: %s", real_name);
+		if(strcmp(normalized_path, "temp") == 0) {
+			strncpy(temp_path, path, 256);
+			temp_path[255] = '\0';
+			obs_log(LOG_INFO, "Recording path is already set to temp directory: %s", temp_path);
+		} else {
+			//strncpy(temp_path, concat(path, "\\temp"), 256);
+			temp_path[255] = '\0';
+			/*if(_mkdir(temp_path) == 0) {
+				obs_log(LOG_INFO, "Created temp directory at: %s", temp_path);
+			} else if (errno == EEXIST) {
+				obs_log(LOG_INFO, "Temp directory already exists at: %s", temp_path);
+			} else {
+				obs_log(LOG_ERROR, "Failed to create temp directory at: %s, error: %d", path, errno);
+				return;
+			}*/
+			obs_log(LOG_INFO, "Setting recording path to temp directory: %s", temp_path);
+		}
 		
-	}
-	obs_output_t *output = obs_get_output_by_name("adv_file_output");
-    if (!output) output = obs_get_output_by_name("simple_file_output");
-	obs_log(LOG_INFO, "method called, PNAMELEN: %s", playing);
-	if (output) {
-		obs_data_t *settings = obs_output_get_settings(output);
-		obs_log(LOG_INFO, "output found: %s", obs_data_get_json(obs_output_get_settings(output)));
-		const char *path = obs_data_get_string(settings, "path");
-		obs_log(LOG_INFO, "current recording path: %s", path);
+		obs_log(LOG_INFO, "Moving recording to temp directory: %s", temp_path);
 	}
 }
 
 void on_event(enum obs_frontend_event event, void *private_data)
 {
-	if(event == OBS_FRONTEND_EVENT_RECORDING_STARTING) {
+	if (event == OBS_FRONTEND_EVENT_RECORDING_STARTING) {
 		obs_log(LOG_INFO, "recording starting event detected");
 
 		obs_output_t *output = obs_frontend_get_recording_output();
 		if (!output) {
-    		obs_log(LOG_WARNING, "No recording output");
-    		return;
+			obs_log(LOG_WARNING, "No recording output");
+			return;
 		}
 		signal_handler_t *sh = obs_output_get_signal_handler(output);
+		if (!sh) {
+			obs_log(LOG_WARNING, "Failed to get signal handler");
+			return;
+		}
 
 		signal_handler_connect(sh, "start", on_recording_start, NULL);
 	}
